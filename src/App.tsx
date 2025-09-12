a26e0b4e730cac5814a51146c4d4784631a977fe
Vishwajeet Mishra <vishwajeetmishra@rocketmail.com>
2025-09-12 14:09:38 -0700

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <React.Fragment>
+      <h1>MY REACT PRACTISE APP</h1>
+    </React.Fragment>
+  )
+}
+
+export default App
